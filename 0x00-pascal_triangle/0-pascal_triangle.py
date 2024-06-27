7f3f504e3d414e6d861a9e50a18082c3eb5b4fb4
kilokomuli <kilokoemma175@gmail.com>
2024-06-27 13:51:23 -0700

    fix linter

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """ defines function  pascal_triangle that
 returns a list of lists of integers"""
+
+
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing the Pascal’s triangle of n.
+    Returns a list of lists of integers representing the
+    Pascal’s triangle of n.
 
     Args:
         n (int): The number of rows in the Pascal's triangle.
@@ -20,6 +23,7 @@ def pascal_triangle(n):
         for j in range(1, i):
             row.append(previous_row[j - 1] + previous_row[j])
         row.append(1)
+
         triangle.append(row)
 
     return triangle
