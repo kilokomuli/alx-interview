0c4f2b78d07cacb442a78360b049e2f56efd5704
kilokomuli <kilokoemma175@gmail.com>
2024-07-07 19:33:17 +0300

    locked boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""canUnlockAll determines if all boxes can be opened"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened.
+
+    Args:
+        boxes (list): A list of lists where each index contains a list of
+        integers representing a box.
+
+    Returns:
+        True: if all boxes can be opened
+        else False
+    """
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    stack = [0]
+
+    while stack:
+        current_box = stack.pop()
+        for key in boxes[current_box]:
+            if key < n and not visited[key]:
+                visited[key] = True
+                stack.append(key)
+
+    return  all(visited)
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,4 @@
+## Locked Boxes
+Learn how to open locked boxes in python. I used a deoth first search approach to explore the boxes and track which boxes have been opened  
+
+Returns True if all boxes can be opened else returns False
