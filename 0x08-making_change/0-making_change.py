7da3331211e45295a30abb24b80171224a9ba1d4
kilokomuli <kilokoemma175@gmail.com>
2024-08-25 13:23:46 -0700

    make change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""MAKECHANGE"""
+
+
+def makeChange(coins, total):
+    """Determines the fewest number of coins needed to meet
+    the given amoount"""
+    if total <= 0:
+        return 0
+    coin = 0
+    tmp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while coin < total:
+            coin += 1
+            tmp += 1
+        if coin == total:
+            return tmp
+        coin -= 1
+        tmp -= 1
+    return -1
