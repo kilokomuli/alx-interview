24fa66307e6ed0189f2aca48271a9d966d158427
Emma <kilokoemmma175@gmail.com>
2024-12-05 23:32:59 -0800

    task1

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""" Island Parameter"""
+
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid
+    Args: grid(list of list of int): The grid representing the island and water.
+    Returns:
+         int: The perimet of the island.
+    """
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                perimeter += 4
+                if r > 0 and grid[r - 1][c] == 1:
+                    perimeter -= 1
+                if r < rows - 1 and grid[r + 1][c] == 1:
+                    perimeter -= 1
+                if c > 0 and grid[r][c - 1] == 1:
+                    perimeter -= 1
+                if c < cols - 1 and grid[r][c + 1] == 1:
+                    perimeter -= 1
+    return perimeter
